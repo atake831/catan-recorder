0c9ab43792a7c9213f24062b769c982ee9eb75ca
Akira Takeguchi <a.takeguchi.sf@gmail.com>
2016-12-17 04:45:39 +0900

    number button

---
+++ src/app/record.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class RecordService {
+  private diceHistory: number[]; 
+
+  constructor() {
+    this.diceHistory = [];
+  }
+
+  public push(v: number) {
+    this.diceHistory.push(v);
+  }
+
+  public historyBack() {
+    this.diceHistory.pop();
+  }
+
+  public getProbability(): { [key: number]: number } {
+    var hashHistory = this.changeArrayToHash();
+    console.log(hashHistory);
+
+    for ( var key in hashHistory ) {
+      hashHistory[key] = hashHistory[key] / this.diceHistory.length * 100;
+    }
+
+    console.log(hashHistory);
+
+    return hashHistory;
+  }
+
+  public getTotalTurns(): number {
+    return this.diceHistory.length;
+  }
+
+  private changeArrayToHash(): { [key: number]: number } {
+    var hashHistory: { [key: number]: number } = {};
+
+    this.diceHistory.forEach( (n) => {
+      hashHistory[n] ? hashHistory[n]++ : hashHistory[n] = 1;
+    });
+
+    return hashHistory;
+  }
+
+}
--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,15 +1,36 @@
 import { Component } from '@angular/core';
+import { RecordService } from '../../app/record.service';
 
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-page1',
+  providers: [RecordService],
   templateUrl: 'page1.html'
 })
 export class Page1 {
+  diceProbability: { [key: number]: number };
+  totalTurns: number;
 
-  constructor(public navCtrl: NavController) {
-    
+  constructor(public navCtrl: NavController, private recordService: RecordService) {
+    this.update();
   }
+
+  select(selectedNumber: number) {
+    console.log(selectedNumber);
+    this.recordService.push(selectedNumber);
+    this.update();
+  }
+
+  back() {
+    this.recordService.historyBack();
+    this.update();
+  }
+
+  private update() {
+    this.diceProbability = this.recordService.getProbability();
+    this.totalTurns = this.recordService.getTotalTurns();
+  }
+
 
 }
